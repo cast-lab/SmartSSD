cf958f4198d7d1f3f6691e2ec70b98b1b4e11672
Ji-Hoon-0708 <jihoon0708@kaist.ac.kr>
2020-01-10 09:12:36 +0900

    bruteforce, hnsw* for HW development

--- Src/hnswlib.h
+++ Src/hnswlib.h
@@ -27,49 +27,97 @@
 
 #include <string.h>
 
+#define MAX_NUM_NODES 1000
+
 namespace hnswlib {
-    typedef size_t labeltype;
 
-    template<typename T>
-    static void writeBinaryPOD(std::ostream &out, const T &podRef) {
-        out.write((char *) &podRef, sizeof(T));
-    }
+	typedef unsigned int tableint;
+	struct Node {
+		int64_t dist;
+		tableint ep_id;
 
-    template<typename T>
-    static void readBinaryPOD(std::istream &in, T &podRef) {
-        in.read((char *) &podRef, sizeof(T));
-    }
+		struct Node* next;
+		struct Node* prev;
 
-    template<typename MTYPE>
-    using DISTFUNC = MTYPE(*)(const void *, const void *, const void *);
+		int8_t is_free;
+	};
+
+	struct NodePointers {
+		Node* head;
+		Node* tail;
+	};
+
+	class StaticPriorityQueue {
+	public:
+		StaticPriorityQueue();
+		Node* GetNewNode();
+		void FreeNode(Node* node);
+		void InitNode(Node* node);
+
+		uint32_t Size() { return num_data_nodes; }
+		uint8_t Empty() { return num_data_nodes == 0; }
+		Node* Emplace(int64_t dist, tableint ep_id);
+		void Pop();
+		Node* Top();
+		void Swap(StaticPriorityQueue& spq);
+
+		void Debug();
+
+	private:
+		Node nodes[MAX_NUM_NODES];
+
+		NodePointers data_list;
+		NodePointers free_list;
+
+		uint32_t num_free_nodes;
+		uint32_t num_data_nodes;
+	};
 
 
-    template<typename MTYPE>
-    class SpaceInterface {
-    public:
-        //virtual void search(void *);
-        virtual size_t get_data_size() = 0;
+	typedef size_t labeltype;
 
-        virtual DISTFUNC<MTYPE> get_dist_func() = 0;
+	template<typename T>
+	static void writeBinaryPOD(std::ostream& out, const T& podRef) {
+		out.write((char*)&podRef, sizeof(T));
+	}
 
-        virtual void *get_dist_func_param() = 0;
+	template<typename T>
+	static void readBinaryPOD(std::istream& in, T& podRef) {
+		in.read((char*)&podRef, sizeof(T));
+	}
 
-        virtual ~SpaceInterface() {}
-    };
+	template<typename MTYPE>
+	using DISTFUNC = MTYPE(*)(const void*, const void*, const void*);
 
-    template<typename dist_t>
-    class AlgorithmInterface {
-    public:
-        virtual void addPoint(void *datapoint, labeltype label)=0;
-        virtual std::priority_queue<std::pair<dist_t, labeltype >> searchKnn(const void *, size_t, size_t) const = 0;
-        virtual void saveIndex(const std::string &location)=0;
-        virtual ~AlgorithmInterface(){
-        }
-    };
+
+	template<typename MTYPE>
+	class SpaceInterface {
+	public:
+		//virtual void search(void *);
+		virtual size_t get_data_size() = 0;
+
+		virtual DISTFUNC<MTYPE> get_dist_func() = 0;
+
+		virtual void* get_dist_func_param() = 0;
+
+		virtual ~SpaceInterface() {}
+	};
+
+	template<typename dist_t>
+	class AlgorithmInterface {
+	public:
+		virtual void addPoint(void* datapoint, labeltype label) = 0;
+	
+		virtual std::priority_queue<std::pair<dist_t, labeltype >> searchKnn_bf(const void*, size_t, size_t) const = 0;
+		virtual StaticPriorityQueue searchKnn(const void*, size_t, size_t) const  = 0;
+
+		virtual void saveIndex(const std::string& location) = 0;
+		virtual ~AlgorithmInterface() {
+		}
+	};
 
 
 }
-
 #include "space_l2.h"
 #include "space_ip.h"
 #include "bruteforce.h"
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,8 +1,11 @@
 
 
-void sift_test1B();
+//void sift_test1B();
+void sift_test();
+
 int main() {
-	sift_test1B();
+	//sift_test1B();
+	sift_test();
 
 	return 0;
-};
+}; 
